65b9e28766134cc4d1469e1d327c498e7bfb8956
Manuu1298 <manuel.ed.roga@gmail.com>
2023-04-10 23:22:01 -0600

    Initial commit

---
+++ parser/parserapi.py
@@ -0,0 +1,43 @@
+import openai
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    text = extract_text(file)
+    summary = summarize_text(text)
+    return render_template('summary.html', summary=summary)
+
+def extract_text(file):
+    """Extract text from a file."""
+    # Assumes the file is a PDF
+    text = ""
+    with openai.file(file.stream) as f:
+        pdf = openai.Compression.gzip_decompress(f.read())
+        text = openai.FormattedText(pdf).plain_text()
+    return text
+
+def summarize_text(text):
+    """Summarize text using OpenAI GPT-3 API."""
+    prompt = (f"Please summarize the following text into 5 sentences:\n\n{text}\n\nSummary:")
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    summary = response.choices[0].text.strip()
+    return summary
+
+if __name__ == '__main__':
+    app.run(debug=True)
