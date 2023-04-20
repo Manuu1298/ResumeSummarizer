cf04ba211c240d0712c53fa665d3f9c397847651
Manuu1298 <manuel.ed.roga@gmail.com>
2023-04-20 08:58:28 -0600

    Summarizer Update

--- app.py
+++ app.py
@@ -41,10 +41,10 @@ def upload_file():
 
         # Render summarized text in HTML format
         
-        return render_template("testresult.html", summary=summary)
+        return render_template("1summarizerresult.html", summary=summary)
  
     
-    return render_template("test.html")
+    return render_template("1summarizer.html")
 
 
 
