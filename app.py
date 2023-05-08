a48775d1def2b2102b18bde18da68cf79e5829c2
Manuu1298 <manuel.ed.roga@gmail.com>
2023-05-08 09:17:19 -0600

    Model Engine Update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import jsonify
  
 app = Flask(__name__)
 openai.api_key = "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"
-model_engine = 'text-davinci-003'
+model_engine = 'text-davinci-002'
 
 async def summarize_text(text):
     response = openai.Completion.create(
